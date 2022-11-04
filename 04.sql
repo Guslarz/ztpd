3208f6e63bd06e3ea389644b8380a43c6b0bd2df
Stanisław Kaczmarek <mrkaczmareks@gmail.com>
2022-11-04 16:05:32 +0100

    Create 04.sql

---
+++ 04.sql
@@ -0,0 +1,58 @@
+-- 1
+DROP TABLE figury;
+CREATE TABLE figury (
+    id NUMBER(1) PRIMARY KEY,
+    ksztalt MDSYS.SDO_GEOMETRY
+);
+
+INSERT INTO figury VALUES (
+    1,
+    MDSYS.SDO_GEOMETRY(
+        2003,
+        NULL,
+        NULL,
+        MDSYS.SDO_ELEM_INFO_ARRAY(1, 1003, 4),
+        MDSYS.SDO_ORDINATE_ARRAY(3,5, 5,7, 7,5)
+    )
+);
+
+INSERT INTO figury VALUES (
+    2,
+    MDSYS.SDO_GEOMETRY(
+        2003,
+        NULL,
+        NULL,
+        MDSYS.SDO_ELEM_INFO_ARRAY(1, 1003, 3),
+        MDSYS.SDO_ORDINATE_ARRAY(1,1, 5,5)
+    )
+);
+
+INSERT INTO figury VALUES (
+    3,
+    MDSYS.SDO_GEOMETRY(
+        2002,
+        NULL,
+        NULL,
+        MDSYS.SDO_ELEM_INFO_ARRAY(1,4,2, 1,2,1, 5,2,2),
+        MDSYS.SDO_ORDINATE_ARRAY(3,2, 6,2, 7,3, 8,2, 7,1)
+    )
+);
+
+INSERT INTO figury VALUES (
+    4,
+    MDSYS.SDO_GEOMETRY(
+        2003,
+        NULL,
+        NULL,
+        MDSYS.SDO_ELEM_INFO_ARRAY(1, 1003, 4),
+        MDSYS.SDO_ORDINATE_ARRAY(1,1, 2,2, 3,3)
+    )
+);
+
+SELECT id, SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(ksztalt, 0.005)
+FROM figury;
+
+DELETE FROM figury
+WHERE SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(ksztalt, 0.005) != 'TRUE';
+
+COMMIT;
